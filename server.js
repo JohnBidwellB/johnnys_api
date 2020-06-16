f7d89576ca95c0215b26680a5fa690cb9463fe7d
John Bidwell <john@foreach.cl>
2020-06-16 10:53:26 -0400

    db conectada

--- server.js
+++ server.js
@@ -16,6 +16,22 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// DB
+const db = require("./models");
+const dbConfig = require("./config/db.config");
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conectado a la base de datos");
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
 // endpoints
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Johnny's api" });
